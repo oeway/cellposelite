1ccb0407189f7b08f20f3d8c717db39acedbe2e2
Wei Ouyang <oeway007@gmail.com>
2022-02-21 12:37:28 +0100

    Update export_torchscript.py

--- export_torchscript.py
+++ export_torchscript.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def export_as_torchscript(model, model_path):
-  example = np.random.randint(0, 255, (1, 2, 256, 256))
-  example = example.to(model.device)
-  traced_model = torch.jit.trace(model.net, torch.from_numpy(example))
+  example = np.random.randint(0, 255, (1, 2, 256, 256)).astype('float32')
+  example = torch.from_numpy(example).to(model.device)
+  traced_model = torch.jit.trace(model.net, example)
   traced_model.save(model_path)
